23eda06bab368805ce2d99a02a4a4b901c993d63
symbioids <parasshresthanever@gmail.com>
2024-05-11 17:13:35 +0545

    first commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import styles from
+
+/***
+ * Quiz between two teams
+ *
+ * team a
+ *
+ * team b
+ *
+ * team a scrore
+ *
+ * team b score
+ */
+
+export function Score(props:{
+    teamName :string;
+    counterName: "Score" | "Goals"
+}) {
+  const [count, setCount] = useState(10);
+
+  const increaseCountBy1 = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCountBy1 = () => {
+    setCount(count - 1);
+  };
+  const resetCountTo0 = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className={styles.counter_container}>
+      <h1>
+        {props.teamName} : {props.counterName}
+      </h1>
+      <div className={styles.counter_container}>
+        <p>{count}</p>
+      </div>
+      <div>
+        <button onClick={increaseCountBy1}>+1</button>
+        <button onClick={decreaseCountBy1}>-1</button>
+        <button onClick={resetCountTo0}>0</button>
+      </div>
+    </div>
+  );
+}
+
+
+export function ScoreB(){
+    const [count, setCount] = useState(10)
+
+    const increaseCountBy1 = () =>{
+        setCount(co)
+    }
+
+}
---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <>
+  <h2 style={{textAlign : "center",
+    padding: "10px",
+    color : "blue"
+
+  }}
+  >Quiz game</h2>
+
+  <div>
+
+  </div>
+  </>
+)
