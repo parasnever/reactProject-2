0267abefe68573030f31495db14f4495e30c68c1
symbioids <parasshresthanever@gmail.com>
2024-05-13 15:38:49 +0545

    second project begining

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export function Navbar(){
+    return(
+        <div
+        style={{
+            display:"flex",
+            justifyContent:"space-between",
+            marginBottom: "10px"
+        }}>
+            <img src="" alt="" />
+            <input type="text" />
+        </div>
+    )
+}
