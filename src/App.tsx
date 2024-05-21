998223bae8c9071dd12ca4265afabf35581a4fa3
symbioids <parasshresthanever@gmail.com>
2024-05-21 18:28:04 +0545

    aja yeti

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar />
+        <Outlet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    ),
+    children: [
+      {
+        path:"/",
+        element:(
+          <div>
+            <PostList />
+          </div>
+        )
+      },
+      {
+        path:"/posts",
+        element:(
+          <div>
+            <PostPaginationProvider>
+              <PostList />
+            </PostPaginationProvider>
+          </div>
+        )
+      },
+      {
+        path: "/posts/:postID",
+        element: <postDetail />
+      },
+      {
+        path:"/profile",
+        element: (
+          <UserCtxProvidrer>
+            <ProfileMainComponent />
+          </UserCtxProvidrer>
+        )
+      }
+    ]
 
+
+  }
+])
+function App(){
+  return <RouterProvider router={router}/>
+
+}
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
+
 import './index.css'
-import { Score } from './components/Counter'
-import styles from "./components/main.module.css"
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <>
-  <h2 style={{textAlign : "center",
-    padding: "10px",
-    color : "blue"
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 
-  }}
-  >Quiz game</h2>
 
-  <div className={styles.scores_container}>
-    <Score teamName='A' counterName='Score'/>
-    <Score teamName='B' counterName='Score'/>
-
-  </div>
-  </>
 )
